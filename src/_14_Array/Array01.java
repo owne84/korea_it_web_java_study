f1557ecbcc888939e93ee896aa90bc3b33166829
owne84 <ufhf0811@gmail.com>
2025-05-20 20:45:01 +0900

    Feat: Array

---
+++ src/_14_Array/Array01.java
@@ -0,0 +1,26 @@
+package _14_Array;
+
+public class Array01 {
+    public static void main(String[] args) {
+        String country1 = "Hungary";
+        String country2 = "Croatia";
+        String country3 = "Slovenia";
+        String country4 = "Austria";
+        String country5 = "Czech";
+
+//        String[] countrys = new String[5];
+//        countrys[0] = country1;
+//        countrys[1] = country2;
+//        countrys[2] = country3;
+//        countrys[3] = country4;
+//        countrys[4] = country5;
+//
+//        System.out.println(countrys[3]);
+
+
+//        String[] conutrys = new String[] {"Hungary", "Croatia", "Slovenia", "Austria", "Czech"};
+
+        String[] countrys = {"Hungary", "Croatia", "Slovenia", "Austria", "Czech"};
+        countrys[1] = "swiss";
+    }
+}
---
+++ src/_14_Array/Array02.java
@@ -0,0 +1,11 @@
+package _14_Array;
+
+public class Array02 {
+    public static void main(String[] args) {
+        String[] countrys = {"Hungary", "Croatia", "Slovenia", "Austria", "Czech"};
+
+        for(int i =0; i < countrys.length; i++) {
+            System.out.println(countrys[i]);
+        }
+    }
+}
