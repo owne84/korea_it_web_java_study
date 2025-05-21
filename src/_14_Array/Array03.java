d829c64c65a418d337813a32f34b94710e1ca51b
owne84 <ufhf0811@gmail.com>
2025-05-21 19:30:43 +0900

    Feat: Array04

---
+++ src/_14_Array/Array03.java
@@ -0,0 +1,18 @@
+package _14_Array;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Array03 {
+    public static void main(String[] args) {
+        int[] nums = {8, 4, 5, 1, 7, 10, 6, 2, 9 ,3};
+
+        System.out.println(Arrays.toString(nums));
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+
+//        Arrays.sort(nums, Comparator.reverseOrder());
+
+
+    }
+}
