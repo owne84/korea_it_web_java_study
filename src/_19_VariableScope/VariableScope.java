89a1b681bf8395e0d6d4e2834be901a7ce0b6c86
owne84 <ufhf0811@gmail.com>
2025-05-22 19:25:46 +0900

    Feat: VariableScope

---
+++ src/_19_VariableScope/VariableScope.java
@@ -0,0 +1,11 @@
+package _19_VariableScope;
+
+public class VariableScope {
+    public static void method() {
+        String str = "abc";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
